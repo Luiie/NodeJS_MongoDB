1514e0c8a19331d99aceda81581a8db6a3051a64
LuiieClone <dbsldkfls@gmail.com>
2024-09-29 14:10:02 +0900

    404 Error for No ID

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,9 +33,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id);
     
-    // if(!tour) {
-    //     return next(new AppError("No tour found with this ID", 404));
-    // }
+    if(!tour) {
+        return next(new AppError("No tour found with this ID", 404));
+    }
 
     res.status(200).json({
         status: "success",
@@ -63,6 +63,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true
     });
 
+    if(!tour) {
+        return next(new AppError("No tour found with this ID", 404));
+    }
+
     res.status(200).json({
     status: 'success',
     data: {
@@ -72,7 +76,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-    await Tour.findByIdAndDelete(req.params.id);
+    const tour = await Tour.findByIdAndDelete(req.params.id);
+
+    if(!tour) {
+        return next(new AppError("No tour found with this ID", 404));
+    }
 
     res.status(204).json({
         status: 'success',
